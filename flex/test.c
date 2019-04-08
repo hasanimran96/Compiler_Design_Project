e7f68a20c07821805d3ccd6a7d09f11aa8776bc0
hasanimran96 <hasanimran_96@yahoo.com>
2019-04-08 20:33:00 +0500

    added flex code

---
+++ flex/test.c
@@ -0,0 +1,27 @@
+int main() {
+    int x;
+    float y;
+
+    y = 32.56;
+    x = 69;
+
+    /*
+     * // fdsfdsfsdgsd
+           fasfasf
+fafaf y = You must not print the stuff in the commments
+ fsffdfdf
+fdfsdfsdfsd
+//   
+  */
+
+    //  Dont print this
+
+    while (x) {
+    x = x/2;
+    }
+
+    if (x == 0) {
+      y = 2;
+    }
+
+}
